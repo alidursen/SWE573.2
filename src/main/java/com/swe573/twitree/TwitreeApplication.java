a1de213910e3d583162ea59e04d26b4ac66ecd19
Ali Kutlu Durşen <alidursen@gmail.com>
2018-12-28 17:39:58 +0300

    Resolving(?) Apache 404 error on Heroku: Per Taner Eşme's instruction, code segment from Mnemosyne (@SWE574) is adapted and pasted.

--- src/main/java/com/swe573/twitree/TwitreeApplication.java
+++ src/main/java/com/swe573/twitree/TwitreeApplication.java
@@ -2,9 +2,16 @@ package com.swe573.twitree;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TwitreeApplication {
+public class TwitreeApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(TwitreeApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(TwitreeApplication.class, args);
